ee2aff926dad9c839cf8f8144296c3df18130979
Luiz Felipe <felipeborgeseng@outlook.com.br>
2022-09-24 13:38:07 -0300

    primeiro commit

---
+++ src/test/java/br/anhembi/tcc/trafficmanagement/TrafficManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.anhembi.tcc.trafficmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
